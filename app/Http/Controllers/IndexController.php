938090f23abb2cb92857fe76525a4dba1553f6ac
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-08 10:50:59 -0300

    recaftor: return inertia rendering into IndexController.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,11 +9,11 @@ class IndexController extends Controller
 {
     public function index()
     {
-        return 'Index';
+        return inertia('Index/Index');
     }
 
     public function show()
     {
-        return 'Show';
+        return inertia('Index/Show');
     }
 }

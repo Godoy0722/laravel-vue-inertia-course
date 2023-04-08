9e9c6de550b7218f419d6482e021f0a1eb98e0b2
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-08 11:31:00 -0300

    refactor: use inertia layout to maintain the state of the MainLayout

--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,11 +1,16 @@
 <template>
-    <MainLayout>
-        <div>Show</div>
-        <Link href="/">IndexPage</Link>
-    </MainLayout>
+    <div>Show</div>
+    <Link href="/">IndexPage</Link>
 </template>
 
 <script setup>
 import {Link} from '@inertiajs/vue3';
-import MainLayout from "@/Layouts/MainLayout.vue";
+</script>
+
+<script>
+import MainLayout from "../../Layouts/MainLayout.vue";
+
+export default {
+    layout: MainLayout
+}
 </script>

a566973792fbece4fc3bb1b408a9e6eccac99e5d
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-08 11:13:04 -0300

    refactor: add MainLayout to Index and Show components.

--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>Show</div>
-    <Link href="/">IndexPage</Link>
+    <MainLayout>
+        <div>Show</div>
+        <Link href="/">IndexPage</Link>
+    </MainLayout>
 </template>
 
 <script setup>
 import {Link} from '@inertiajs/vue3';
+import MainLayout from "@/Layouts/MainLayout.vue";
 </script>

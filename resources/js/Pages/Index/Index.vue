9fb33ecace9e0b0cae3e1610aa26d9e8688974f5
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-08 11:06:26 -0300

    refactor: Use data passed through IndexController in Index component.

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>Index</div>
+    <div>Index</div>
     <Link href="/hello">ShowPage</Link>
+    <div>The message is {{ message }}</div>
 </template>
 
 <script setup>
     import {Link} from '@inertiajs/vue3';
+
+    defineProps([{
+        message: String
+    }]);
+
 </script>

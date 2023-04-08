0801ff632641c4219a473716a6a22f476963c422
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-08 10:51:36 -0300

    refactor: Add Link component through inertia in components Index and Show.

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,3 +1,8 @@
 <template>
   <div>Index</div>
+    <Link href="/hello">ShowPage</Link>
 </template>
+
+<script setup>
+    import {Link} from '@inertiajs/vue3';
+</script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,3 +1,8 @@
 <template>
     <div>Show</div>
+    <Link href="/">IndexPage</Link>
 </template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3';
+</script>

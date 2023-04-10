805df187957b15090f07432a2b723343fe872155
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-10 15:54:31 -0300

    refactor: update listing Index and Show components.

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-for="listing in listings" :key="`listing_${listing.id}`">
+    <Link :href="`/listings/${listing.id}`">
+      <ListingAddress :listing="listing" />
+    </Link>
+  </div>
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3'
+import ListingAddress from '@/Components/ListingAddress.vue'
+defineProps({
+    listings: Array,
+})
+</script>
---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,11 @@
+<template>
+  <ListingAddress :listing="listing" />
+</template>
+
+<script setup>
+import ListingAddress from '@/Components/ListingAddress.vue'
+
+defineProps({
+    listing: Object,
+})
+</script>

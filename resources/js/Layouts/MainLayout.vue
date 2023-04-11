2aa3ddd1c16467d2cedc4898c3ebd5a600d869fc
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-11 16:02:53 -0300

    refactor: add flash message in MainLayout

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,15 +2,22 @@
   <Link href="/">Main Page</Link>&nbsp;
   <Link href="/hello">Show Page</Link>
 
-  <div>The page with time {{ timer }}</div>
-
-  <slot />
+  <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+  </div>
+  <slot>Default</slot>
 </template>
 
 <script setup>
 import {Link} from '@inertiajs/vue3'
-import {ref} from 'vue'
+import {computed} from 'vue'
 
-const timer = ref(0)
-setInterval(() => timer.value++, 1000)
+const flashSuccess = computed(() => this.$page.props.flash.success)
 </script>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: white;
+}
+</style>

619eca833113bec0aa62532b4ac856c8c0869760
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-12 16:32:15 -0300

    config: add ziggy configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from  '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from  '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -12,7 +13,12 @@ export default defineConfig({
             template: {
                 base: null,
                 includeAbsolute: false,
-            }
-        })
+            },
+        }),
     ],
-});
+    resolve: {
+        alias: {
+            ziggy: path.resolve('vendor/tightenco/ziggy/dist/vue.es.js'),
+        },
+    },
+})

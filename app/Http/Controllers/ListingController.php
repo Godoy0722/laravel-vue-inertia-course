8b6366a0a00576c812b953d5d6d308b550d35f98
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-12 15:59:24 -0300

    refactor: implement update methods.

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -8,25 +8,16 @@ use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return inertia('Listing/Index', ['listings' => Listing::all()]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return inertia('Listing/Create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         Listing::create($request->validate([
@@ -44,33 +35,33 @@ class ListingController extends Controller
             ->with('success', 'Listing was created');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Listing $listing)
     {
         return inertia('Listing/Show', ['listing' => $listing]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function edit(Listing $listing)
     {
-        //
+        return inertia('Listing/Edit', ['listing' => $listing]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Listing $listing)
     {
-        //
+        $listing->update($request->validate([
+            'beds' => 'required|integer|min:0|max:20',
+            'baths' => 'required|integer|min:0|max:20',
+            'area' => 'required|integer|min:15|max:1500',
+            'city' => 'required',
+            'code' => 'required',
+            'street' => 'required',
+            'street_nr' => 'required|min:1|max:10000',
+            'price' => 'required|integer|min:1|max:20000000',
+        ]));
+
+        return redirect()->route('listings.index')
+            ->with('success', 'Listing was updated');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //

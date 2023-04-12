03c5e9c72a8700c1dc6aa1016bf054731a66b230
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-12 16:00:39 -0300

    fix: bug for usePage implemented.

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <Link href="/">Main Page</Link>&nbsp;
-  <Link href="/hello">Show Page</Link>
+  <Link href="/listings">Listings</Link>&nbsp;
+  <Link href="/listings/create">New Listing</Link>
 
   <div v-if="flashSuccess" class="success">
     {{ flashSuccess }}
@@ -12,9 +12,7 @@
 import {Link, usePage} from '@inertiajs/vue3'
 import {computed} from 'vue'
 
-const page = usePage()
-
-const flashSuccess = computed(() => page.props.flash.success)
+const flashSuccess = computed(() => usePage().props.flash.success)
 </script>
 
 <style scoped>

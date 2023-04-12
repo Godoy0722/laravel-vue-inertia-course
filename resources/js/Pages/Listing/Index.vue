ff09f06de6c265e233756141c696c95dab8dd3cd
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-12 16:09:50 -0300

    refactor: add delete Link component for listings.

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -8,12 +8,16 @@
     <div>
       <Link :href="`/listings/${listing.id}/edit`">Edit</Link>
     </div>
+    <div>
+      <Link :href="`/listings/${listing.id}`" method="DELETE">Delete</Link>
+    </div>
   </div>
 </template>
 
 <script setup>
 import {Link} from '@inertiajs/vue3'
 import ListingAddress from '@/Components/ListingAddress.vue'
+
 defineProps({
     listings: Array,
 })

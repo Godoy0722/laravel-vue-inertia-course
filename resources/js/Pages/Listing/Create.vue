4d480414cf1176cc37b4c9811f45c1e8402831ca
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-12 15:15:32 -0300

    refactor: use InputErrorMessage component to show form validation errors.

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -4,41 +4,49 @@
       <div>
         <label>Beds</label>
         <input v-model.number="form.beds" type="text" />
+        <InputErrorMessage :error="form.errors.beds" />
       </div>
 
       <div>
         <label>Baths</label>
         <input v-model.number="form.baths" type="text" />
+        <InputErrorMessage :error="form.errors.baths" />
       </div>
 
       <div>
         <label>Area</label>
         <input v-model.number="form.area" type="text" />
+        <InputErrorMessage :error="form.errors.area" />
       </div>
 
       <div>
         <label>City</label>
         <input v-model="form.city" type="text" />
+        <InputErrorMessage :error="form.errors.city" />
       </div>
 
       <div>
         <label>Post Code</label>
         <input v-model="form.code" type="text" />
+        <InputErrorMessage :error="form.errors.code" />
       </div>
 
       <div>
         <label>Street</label>
         <input v-model="form.street" type="text" />
+        <InputErrorMessage :error="form.errors.street" />
       </div>
 
       <div>
         <label>Street Nr</label>
         <input v-model.number="form.street_nr" type="text" />
+        <InputErrorMessage :error="form.errors.street_nr" />
       </div>
 
       <div>
         <label>Price</label>
         <input v-model.number="form.price" type="text" />
+        <InputErrorMessage :error="form.errors.price" />
       </div>
 
       <div>
@@ -50,6 +58,7 @@
 
 <script setup>
 import {useForm} from '@inertiajs/vue3'
+import InputErrorMessage from '@/Components/InputErrorMessage.vue'
 
 const form = useForm({
     beds: 0,

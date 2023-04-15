5a5bf12c7c886137aa5518a9dbc23b313bc2b0a4
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-15 10:15:36 -0300

    config: create tailwind initial config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './storage/framweork/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+

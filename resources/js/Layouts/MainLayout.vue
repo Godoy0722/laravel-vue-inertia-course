14078e2f857a2b4176dcdfdebf103f55bd3e9906
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-15 11:25:37 -0300

    style: update header's layout.

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,11 +1,28 @@
 <template>
-  <Link :href="route('listings.index')">Listings</Link>&nbsp;
-  <Link :href="route('listings.create')">New Listing</Link>
+  <header class="border-b border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white w-full">
+    <div class="container mx-auto">
+      <nav class="p-4 flex items-center justify-between">
+        <div class="text-lg font-medium">
+          <Link :href="route('listings.index')">Listings</Link>
+        </div>
+        <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+          <Link :href="route('listings.index')">LaraZillow</Link>
+        </div>
+        <div>
+          <Link :href="route('listings.create')" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">
+            + New Listing
+          </Link>
+        </div>
+      </nav>
+    </div>
+  </header>
 
-  <div v-if="flashSuccess" class="success">
-    {{ flashSuccess }}
-  </div>
-  <slot>Default</slot>
+  <main class="container mx-auto p-4">
+    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+      {{ flashSuccess }}
+    </div>
+    <slot>Default</slot>
+  </main>
 </template>
 
 <script setup>
@@ -14,10 +31,3 @@ import {computed} from 'vue'
 
 const flashSuccess = computed(() => usePage().props.flash.success)
 </script>
-
-<style scoped>
-.success {
-    background-color: green;
-    color: white;
-}
-</style>

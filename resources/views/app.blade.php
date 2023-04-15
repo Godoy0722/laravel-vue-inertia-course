2d637a0d4357188409d332aa5e6851ebebfcb088
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-15 10:43:08 -0300

    refactor: add first tailwind classes to the project.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     </head>
 
-    <body>
+    <body class="bg-white dark:bg-gray-900">
     @inertia
     </body>
 </html>

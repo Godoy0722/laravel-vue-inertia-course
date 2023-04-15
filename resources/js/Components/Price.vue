5b262cdd88d7d6da62e833f834649d69db93436c
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-15 12:58:44 -0300

    feat: create Price component.

---
+++ resources/js/Components/Price.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>{{ formattedPrice }}</span>
+</template>
+
+<script setup>
+import {computed} from 'vue'
+
+const props = defineProps({ price: [Number, String] })
+
+const formattedPrice  = computed(
+    () => Number(props.price).toLocaleString('en-UK', {
+        style: 'currency',
+        currency: 'GBP',
+        maximumFractionDigits: 0,
+    }),
+)
+</script>

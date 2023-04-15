9591f50e8625f70ce3ffc0b4a9c740ffadf96161
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-15 11:29:58 -0300

    refactor: use Box component and add grid tailwind styles through items

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,22 +1,25 @@
 <template>
-  <div v-for="listing in listings" :key="`listing_${listing.id}`">
-    <div>
-      <Link :href="route('listings.show', { listing: listing.id })">
-        <ListingAddress :listing="listing" />
-      </Link>
-    </div>
-    <div>
-      <Link :href="route('listings.edit', { listing: listing.id })">Edit</Link>
-    </div>
-    <div>
-      <Link :href="route('listings.destroy', { listing: listing.id })" method="DELETE" as="button">Delete</Link>
-    </div>
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+    <Box v-for="listing in listings" :key="`listing_${listing.id}`">
+      <div>
+        <Link :href="route('listings.show', { listing: listing.id })">
+          <ListingAddress :listing="listing" />
+        </Link>
+      </div>
+      <div>
+        <Link :href="route('listings.edit', { listing: listing.id })">Edit</Link>
+      </div>
+      <div>
+        <Link :href="route('listings.destroy', { listing: listing.id })" method="DELETE" as="button">Delete</Link>
+      </div>
+    </Box>
   </div>
 </template>
 
 <script setup>
 import {Link} from '@inertiajs/vue3'
 import ListingAddress from '@/Components/ListingAddress.vue'
+import Box from '@/Components/UI/Box.vue'
 
 defineProps({
     listings: Array,

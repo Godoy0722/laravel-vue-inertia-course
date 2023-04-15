f957db197e401cbdb9703d4c89c0ab24cdc37ca3
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-15 11:44:12 -0300

    feat: create ListingSpace component.

---
+++ resources/js/Components/ListingSpace.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <span class="font-bold">{{ listing.beds }}</span> bds <span class="text-gray-400">|</span>&nbsp;
+    <span class="font-bold">{{ listing.baths }}</span> ba <span class="text-gray-400">|</span>&nbsp;
+    <span class="font-bold">{{ listing.area }}</span> m²
+  </div>
+</template>
+
+<script setup>
+defineProps({ listing: Object })
+</script>

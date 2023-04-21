a94a99187d6ecd801aae3e623742ab4d7c846618
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-21 13:56:53 -0300

    feat: create range inputs to modify the montlhy payment of a listing.

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -16,9 +16,33 @@
 
       <Box>
         <template #header>
-          Offer
+          Monthly Payment
         </template>
-        Make an offer
+        <div>
+          <label class="label">Interest Rate ({{ interestRate }}%)</label>
+          <input
+            v-model.number="interestRate"
+            type="range"
+            min="0.1"
+            max="30"
+            step="0.1"
+            class="range-input"
+          />
+          <label class="label">Duration ({{ duration }} years)</label>
+          <input
+            v-model.number="duration"
+            type="range"
+            min="3"
+            max="35"
+            step="1"
+            class="range-input"
+          />
+
+          <div class="text-gray-600 dark:text-gray-300 mt-2">
+            <div class="text-gray-400">Your monthly payment</div>
+            <Price :price="monthlyPayment" class="text-3xl" />
+          </div>
+        </div>
       </Box>
     </div>
   </div>
@@ -29,8 +53,20 @@ import ListingAddress from '@/Components/ListingAddress.vue'
 import Price from '@/Components/Price.vue'
 import ListingSpace from '@/Components/ListingSpace.vue'
 import Box from '@/Components/UI/Box.vue'
+import {computed, ref} from 'vue'
 
-defineProps({
+const interestRate = ref(2.5)
+const duration = ref(25)
+
+const props = defineProps({
     listing: Object,
+})
+
+const monthlyPayment = computed(() => {
+    const principle = props.listing.price
+    const monthlyInterest = interestRate.value / 100 / 12
+    const numberOfPaymentMonths = duration.value * 12
+
+    return principle * monthlyInterest * (Math.pow(1 + monthlyInterest, numberOfPaymentMonths)) / (Math.pow(1 + monthlyInterest, numberOfPaymentMonths) - 1)
 })
 </script>

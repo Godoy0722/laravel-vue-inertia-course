1b8915c0f5937482c94f010060149c0872b0971b
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-22 13:01:32 -0300

    feat: create AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}

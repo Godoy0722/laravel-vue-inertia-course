dcfc8e9415d076aca61fddaa25a6ff403446acef
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-22 15:05:49 -0300

    feat: create Login component.

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="login()">
+    <div class="w-1/2 mx-auto">
+      <div>
+        <label for="email" class="label">E-mail (username)</label>
+        <input id="email" v-model="form.email" type="text" class="input" />
+        <InputErrorMessage error="Potential Error" />
+      </div>
+
+      <div class="mt-4">
+        <label for="password" class="label">Password</label>
+        <input id="password" v-model="form.password" type="password" class="input" />
+      </div>
+      <div class="mt-4">
+        <button class="btn-primary w-full" type="submit">Login</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script setup>
+import InputErrorMessage from '@/Components/InputErrorMessage.vue'
+import {useForm} from '@inertiajs/vue3'
+
+const form = useForm({
+    email: null,
+    password: null,
+})
+
+const login = () => form.post(route('login.store'), form)
+</script>

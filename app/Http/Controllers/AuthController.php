cdb45762f392afcc1fdda061454b76ef229a2248
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-24 08:09:36 -0300

    refactor: update destroy method to logout session.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,8 +29,13 @@ class AuthController extends Controller
         return redirect()->intended('/listings');
     }
 
-    public function destroy()
+    public function destroy(Request $request)
     {
+        Auth::logout();
 
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('listings.index');
     }
 }

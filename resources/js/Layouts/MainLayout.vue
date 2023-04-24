9585d5f5d3f981836b89cf47cff36781224c5c0f
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-24 10:48:38 -0300

    refactor: add a Register button that redirects to Create user account's page.

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -20,7 +20,8 @@
           </div>
         </div>
 
-        <div v-else>
+        <div v-else class="flex items-center gap-2">
+          <Link :href="route('user-account.create')">Register</Link>
           <Link :href="route('login')">Sign-In</Link>
         </div>
       </nav>

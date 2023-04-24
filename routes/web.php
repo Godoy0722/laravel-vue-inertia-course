8a6c37337492963c5f66272e3a4a3fd3f85454c8
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-24 10:47:35 -0300

    refactor: add create method from UserAccountController in web routes.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,5 +30,8 @@ Route::resource('listings', ListingController::class)
 Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
+
+Route::resource('user-account', UserAccountController::class)
+    ->only(['create']);
 
 

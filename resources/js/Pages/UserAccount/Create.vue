b7c2fcc8a0339582e1e5e3e94ba78e632f26b67b
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-04-24 15:39:45 -0300

    refactor: add link for Login page in Register page.

--- resources/js/Pages/UserAccount/Create.vue
+++ resources/js/Pages/UserAccount/Create.vue
@@ -28,13 +28,17 @@
       <div class="mt-4">
         <button class="btn-primary w-full" type="submit">Create Account</button>
       </div>
+
+      <div class="mt-4 text-center">
+        <Link :href="route('login')" class="text-sm text-gray-500">Already have an account? Click here</Link>
+      </div>
     </div>
   </form>
 </template>
 
 <script setup>
 import InputErrorMessage from '@/Components/InputErrorMessage.vue'
-import {useForm} from '@inertiajs/vue3'
+import {Link, useForm} from '@inertiajs/vue3'
 
 const form = useForm({
     name: null,

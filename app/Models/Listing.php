4a19b233ec4aac8bd8096439ad2991806c73a475
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-05-03 09:51:10 -0300

    refactor: add scopes to Listing model.

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Query\Builder;
 
 class Listing extends Model
 {
@@ -23,5 +24,30 @@ class Listing extends Model
 
     public function owner(): BelongsTo {
         return $this->belongsTo(\App\Models\User::class, 'by_user_id');
+    }
+
+    public function scopeMostRecent(Builder $query): void {
+        $query->orderByDesc('created_at');
+    }
+
+    public function scopeFilters(Builder $query, array $filters): void {
+        $query->when(
+            $filters['priceFrom'] ?? false,
+            fn ($query, $value) => $query->where('price', '>=', $value))
+            ->when(
+                $filters['priceTo'] ?? false,
+                fn ($query, $value) => $query->where('price', '<=', $value))
+            ->when(
+                $filters['beds'] ?? false,
+                fn ($query, $value) => $query->where('beds', (int) $value < 6 ? '=' : '>=', $value))
+            ->when(
+                $filters['baths'] ?? false,
+                fn ($query, $value) => $query->where('baths', (int) $value < 6 ? '=' : '>=', $value))
+            ->when(
+                $filters['areaFrom'] ?? false,
+                fn ($query, $value) => $query->where('area', '>=', $filters['areaFrom']))
+            ->when(
+                $filters['areaTo'] ?? false,
+                fn ($query, $value) => $query->where('area', '<=', $filters['areaTo']));
     }
 }

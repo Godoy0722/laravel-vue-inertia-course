6524a79693c07fff47d4e96ba936657cf06fc98e
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-05-04 10:34:31 -0300

    feat: create Realtor Index component

---
+++ resources/js/Pages/Realtor/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1 class="text-3xl mb-4">Your Listings</h1>
+
+  <section class="mb-4">Filters</section>
+
+  <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
+    <Box v-for="listing in listings" :key="`listing_${listing.id}`">
+      <div class="flex flex-col md:flex-row gap-2 md:items-center justify-between">
+        <div>
+          <div class="xl:flex items-center gap-2">
+            <Price :price="listing.price" class="text-2xl font-medium" />
+            <ListingSpace :listing="listing" />
+          </div>
+
+          <ListingAddress :listing="listing" class="text-gray-500" />
+        </div>
+        <div class="flex item-center gap-1 text-gray-600 dark:text-gray-300">
+          <Link class="btn-outline text-xs font-medium">Preview</Link>
+          <Link class="btn-outline text-xs font-medium">Edit</Link>
+          <Link class="btn-outline text-xs font-medium">Delete</Link>
+        </div>
+      </div>
+    </Box>
+  </section>
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3'
+import Box from '@/Components/UI/Box.vue'
+import Price from '@/Components/Price.vue'
+import ListingSpace from '@/Components/ListingSpace.vue'
+import ListingAddress from '@/Components/ListingAddress.vue'
+
+defineProps({
+    listings: Array,
+})
+</script>

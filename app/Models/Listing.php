bba6722293fbdb9149a81e826b8642cb3b0ef5a0
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-05-04 10:36:27 -0300

    fix: Builder dependency import

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder;
 
 class Listing extends Model
 {

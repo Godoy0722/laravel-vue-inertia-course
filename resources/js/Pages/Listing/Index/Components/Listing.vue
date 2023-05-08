b70f31c8feec1e44879f88e18c7ebe8797a7d587
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-05-08 08:23:54 -0300

    refactor: remove delete button for listings view.

--- resources/js/Pages/Listing/Index/Components/Listing.vue
+++ resources/js/Pages/Listing/Index/Components/Listing.vue
@@ -15,9 +15,6 @@
     <div>
       <Link :href="route('listings.edit', { listing: listing.id })">Edit</Link>
     </div>
-    <div>
-      <Link :href="route('listings.destroy', { listing: listing.id })" method="DELETE" as="button">Delete</Link>
-    </div>
   </Box>
 </template>
 

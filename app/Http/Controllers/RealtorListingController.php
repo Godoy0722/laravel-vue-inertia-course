7eed9337128a0cd0fddaadd08f656a8c8c1809a3
Guilherme Henrique <guilherme.godoy@maximatech.com.br>
2023-05-08 08:27:57 -0300

    feat: add destroy method for RealtorListingController and policy for listings in the same controller.

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class RealtorListingController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Listing::class, 'listing');
+    }
+
     public function index()
     {
         return inertia(
@@ -16,5 +22,13 @@ class RealtorListingController extends Controller
                 'listings' => Auth::user()->listings
             ]
         );
+    }
+
+    public function destroy(Listing $listing)
+    {
+        $listing->deleteOrFail();
+
+        return redirect()->back()
+            ->with('success', 'Listing was deleted');
     }
 }

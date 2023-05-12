5e971f6c063b08e46d3cade9cfba8375dc3ae0c2
Henrique523 <guilherme.hlg.eng@gmail.com>
2023-05-12 08:06:57 -0300

    refactor: use RealtorFilters component in Realtor Index page.

--- resources/js/Pages/Realtor/Index.vue
+++ resources/js/Pages/Realtor/Index.vue
@@ -1,7 +1,9 @@
 <template>
   <h1 class="text-3xl mb-4">Your Listings</h1>
 
-  <section class="mb-4">Filters</section>
+  <section class="mb-4">
+    <RealtorFilters />
+  </section>
 
   <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
     <Box v-for="listing in listings" :key="`listing_${listing.id}`">
@@ -37,6 +39,7 @@ import Box from '@/Components/UI/Box.vue'
 import Price from '@/Components/Price.vue'
 import ListingSpace from '@/Components/ListingSpace.vue'
 import ListingAddress from '@/Components/ListingAddress.vue'
+import RealtorFilters from '@/Pages/Realtor/Components/RealtorFilters.vue'
 
 defineProps({
     listings: Array,

51d085607d0935ce51f7110bd94e40d9704070f0
Henrique523 <guilherme.hlg.eng@gmail.com>
2023-05-12 08:06:35 -0300

    feat: create initial filter component for Realtor Index page.

---
+++ resources/js/Pages/Realtor/Components/RealtorFilters.vue
@@ -0,0 +1,10 @@
+<template>
+  <form>
+    <div class="my-4 flex flex-wrap gap-2">
+      <div class="flex flex-nowrap items-center gap-2">
+        <input id="deleted" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+        <label for="deleted">Deleted</label>
+      </div>
+    </div>
+  </form>
+</template>

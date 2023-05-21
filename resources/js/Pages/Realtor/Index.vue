c401d9921d1177c773d5244d3e02e449a21eafff
Guilherme Henrique <guilherme.hlg.eng@gmail.com>
2023-05-21 14:30:45 -0300

    refactor: add pagination component to realtor index component.

--- resources/js/Pages/Realtor/Index.vue
+++ resources/js/Pages/Realtor/Index.vue
@@ -6,7 +6,7 @@
   </section>
 
   <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
-    <Box v-for="listing in listings" :key="`listing_${listing.id}`">
+    <Box v-for="listing in listings.data" :key="`listing_${listing.id}`">
       <div class="flex flex-col md:flex-row gap-2 md:items-center justify-between">
         <div>
           <div class="xl:flex items-center gap-2">
@@ -17,8 +17,8 @@
           <ListingAddress :listing="listing" class="text-gray-500" />
         </div>
         <div class="flex item-center gap-1 text-gray-600 dark:text-gray-300">
-          <Link class="btn-outline text-xs font-medium">Preview</Link>
-          <Link class="btn-outline text-xs font-medium">Edit</Link>
+          <Link class="btn-outline text-xs font-medium" as="button">Preview</Link>
+          <Link class="btn-outline text-xs font-medium" as="button">Edit</Link>
           <Link
             class="btn-outline text-xs font-medium"
             :href="route('realtor.listing.destroy', { listing: listing.id })"
@@ -31,6 +31,10 @@
       </div>
     </Box>
   </section>
+
+  <section v-if="listings.data.length" class="w-full flex justify-center my-4">
+    <Pagination :links="listings.links" />
+  </section>
 </template>
 
 <script setup>
@@ -40,9 +44,10 @@ import Price from '@/Components/Price.vue'
 import ListingSpace from '@/Components/ListingSpace.vue'
 import ListingAddress from '@/Components/ListingAddress.vue'
 import RealtorFilters from '@/Pages/Realtor/Components/RealtorFilters.vue'
+import Pagination from '@/Components/UI/Pagination.vue'
 
 defineProps({
-    listings: Array,
+    listings: Object,
     filters: Object,
 })
 </script>

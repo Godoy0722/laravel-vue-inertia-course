6c12f4850444af0d248d98aaaa09177b98d8175a
Guilherme Henrique <guilherme.hlg.eng@gmail.com>
2023-05-21 14:24:26 -0300

    refactor: add more filters to do in realtor filters. Fix query params problem with new router provided by inertia/vue3

--- resources/js/Pages/Realtor/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Components/RealtorFilters.vue
@@ -10,20 +10,56 @@
         />
         <label for="deleted">Deleted</label>
       </div>
+
+      <div>
+        <select v-model="filterForm.by" class="input-filter-l w-24">
+          <option value="created_at">Added</option>
+          <option value="price">Price</option>
+        </select>
+        <select v-model="filterForm.order" class="input-filter-r w-32">
+          <option
+            v-for="option in sortOptions"
+            :key="`option_${option.value}`"
+            :value="option.value"
+          >
+            {{ option.label }}
+          </option>
+        </select>
+      </div>
     </div>
   </form>
 </template>
 
 <script setup>
-import {reactive, watch} from 'vue'
-import {Inertia} from '@inertiajs/inertia'
+import {reactive, watch, computed} from 'vue'
+import {router} from '@inertiajs/vue3'
 import {debounce} from 'lodash'
 
-const filterForm = reactive({
-    deleted: false,
+const props = defineProps({
+    filters: Object,
 })
 
-watch(filterForm, debounce(() => Inertia.get(route('realtor.listing.index'), filterForm, {
+const sortLabels = {
+    created_at: [
+        { label: 'Latest', value: 'desc' },
+        { label: 'Oldest', value: 'asc' },
+    ],
+    price: [
+        { label: 'Pricey', value: 'desc' },
+        { label: 'Cheapest', value: 'asc' },
+
+    ],
+}
+
+const sortOptions = computed(() => sortLabels[filterForm.by])
+
+const filterForm = reactive({
+    deleted: props.filters.deleted ?? false,
+    by: props.filters.by ?? 'created_at',
+    order: props.filters.order ?? 'desc',
+})
+
+watch(filterForm, debounce(() => router.get(route('realtor.listing.index'), filterForm, {
     preserveState: true,
     preserveScroll: true,
 }), 1000))

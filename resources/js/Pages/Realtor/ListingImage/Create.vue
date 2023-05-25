ecc102de23fbeebf9b4225c1de2920e59b7ef766
Guilherme Henrique <guilherme.hlg.eng@gmail.com>
2023-05-25 09:03:29 -0300

    feat: add a new section to show images from the listings

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -14,6 +14,16 @@
       </section>
     </form>
   </Box>
+
+  <Box v-if="listing.images.length" class="mt-4">
+    <template #header>Current Listing Images</template>
+
+    <section class="mt-4 grid grid-cols-3 gap-4">
+      <div v-for="image in listing.images" :key="image.id">
+        <img :src="image.src" class="rounded-md" alt="Image" />
+      </div>
+    </section>
+  </Box>
 </template>
 
 <script setup>
